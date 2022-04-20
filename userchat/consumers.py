e8e7b8515391f906a0f8cef7d9400b2b01a9a9c0
ClownDev <clowndev2021@gmail.com>
2022-04-20 18:46:26 +0530

    user to user channel_group_add added

--- userchat/consumers.py
+++ userchat/consumers.py
@@ -3,24 +3,18 @@ import imp
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
+from channels.db import database_sync_to_async
 from django.contrib.auth.models import User
 
 class UserChatConsumer(AsyncWebsocketConsumer):
     
     async def connect(self):
-        targetUser = self.scope['url_route']['kwargs']['username']
+        
         authenticatedUser = self.scope["user"]
-        print(targetUser, authenticatedUser)
-
-        self.group_name = "{}".format(targetUser)
-        self.authenticatedUser = "{}".format(authenticatedUser)
-
+        self.authenticated_user_chat_room = f'user_chat_{authenticatedUser}'
+        print('self.channel_name- ', self.channel_name, self.channel_layer)
         await self.channel_layer.group_add(
-            self.group_name,
-            self.channel_name
-        )
-        await self.channel_layer.group_add(
-            self.authenticatedUser,
+            self.authenticated_user_chat_room,
             self.channel_name
         )
 
@@ -28,7 +22,7 @@ class UserChatConsumer(AsyncWebsocketConsumer):
     
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
-            self.group_name,
+            self.authenticated_user_chat_room,
             self.channel_name
         )
     
@@ -36,23 +30,64 @@ class UserChatConsumer(AsyncWebsocketConsumer):
         data = json.loads(text_data)
         message = data['message']
         username = data['username']
+        sent_by_user = data['sent_by_user']
+        sent_to_user = data['sent_to_user']
+
+        if not message:
+            print('Error:: empty message')
+            return False
+            
+        sent_by_user = await self.get_user_object(sent_by_user)
+        sent_to_user = await self.get_user_object(sent_to_user)
+
+        if not sent_by_user:
+            print('Error:: sent by user is incorrect')
+        if not sent_to_user:
+            print('Error:: send to user is incorrect')
 
         await self.channel_layer.group_send(
-            self.group_name,
+            self.authenticated_user_chat_room,
             {
                 'type': 'recieve_group_message',
                 'message': message,
-                'username': username
+                'username': username,
+                'sent_by_user': sent_by_user,
+                'sent_to_user': sent_to_user
+            }
+        )
+
+        self.other_user_chat_room = f'user_chat_{sent_to_user}'
+        await self.channel_layer.group_send(
+            self.other_user_chat_room,
+            {
+                'type': 'recieve_group_message',
+                'message': message,
+                'username': username,
+                'sent_by_user': sent_by_user,
+                'sent_to_user': sent_to_user
             }
         )
     
     async def recieve_group_message(self, event):
         message = event['message']
         username = event['username']
+        sent_by_user = event['sent_by_user']
+        sent_to_user = event['sent_to_user']
 
         # Send message to WebSocket
         await self.send(
-             text_data=json.dumps({
+            text_data=json.dumps({
             'message': message,
-            'username': username
+            'username': username,
+            'sent_by_user': sent_by_user,
+            'sent_to_user': sent_to_user
         }))
+    
+    @database_sync_to_async
+    def get_user_object(self, user_id):
+        qs = User.objects.filter(id=user_id)
+        if qs.exists():
+            obj = qs.first()
+        else:
+            obj = None
+        return str(obj)
